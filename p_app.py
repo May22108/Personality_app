4cd23e4096fad4027ff1fd96642b740a7d0bdbe5
May Phuu Thwel <mayphuuthwel@parami.edu.mm>
2026-01-22 15:11:13 +0630

    Update p_app.py

--- p_app.py
+++ p_app.py
@@ -8,9 +8,9 @@ def load_model():
         model = pickle.load(f)
     return model
 model = load_model()
-st.title("Personality Prediction Web App (Introvert vs Extrovert)")
+st.title("Personality Prediction Web App (Are you an introvert or extrovert?)")
 st.write("Logistic Regression Model with Pipeline")
-st.markdown("Enter the behavioral details below to predict the personality.")
+st.markdown("Enter the behavioral details and time spent below to predict the personality.")
 
 
 logo_path = "PU_logo.png"
@@ -23,13 +23,13 @@ st.sidebar.markdown("**Course Name:** Introduction to Machine Learning")
 st.sidebar.markdown("**Instructor Name:** Professor Nwe Nwe Htay Win")
 
 Time_spent_Alone = st.number_input("Time spent alone per day (in hours)", min_value = 0.0, max_value = 11.0)
-Social_event_attendance = st.number_input("Social event attendance per month", min_value = 0.0, max_value = 10.0)
-Going_outside = st.number_input("How often do you go outside per week?",min_value = 0.0, max_value = 7.0)
-Friends_circle_size = st.slider("How many close friends do you have in general?",min_value = 0.0, max_value = 15.0)
-Post_frequency = st.slider("How many posts do you upload on social media per week?",min_value = 0.0, max_value = 10.0)
+Social_event_attendance = st.slider("Social event attendance score per month", min_value = 0.0, max_value = 10.0)
+Going_outside = st.slider("How often do you go outside per week?",min_value = 0.0, max_value = 7.0)
+Friends_circle_size = st.number_input("How many close friends do you have in general?",min_value = 0.0, max_value = 15.0)
+Post_frequency = st.number_input("How many posts do you upload on social media per week?",min_value = 0.0, max_value = 10.0)
 
-Stage_fear = st.selectbox("Do you have Stage Fear?", ["Yes","No"])
-Drained_after_socializing = st.selectbox("Do you feel drained after socializing?",["Yes","No"])
+Stage_fear = st.radio("Do you have Stage Fear?", ["Yes","No"])
+Drained_after_socializing = st.checkbox("Do you feel drained after socializing?",["Yes","No"])
 
 user_data ={
     "Time_spent_Alone": Time_spent_Alone,
@@ -65,6 +65,7 @@ probability = model.predict_proba(input_df)[0][1]
 st.write("### Prediction Probability")
 st.progress(probability)
 st.write(f"{probability*100:.2f}% chance of being {prediction}")
+
 
 
 
