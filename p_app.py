77612b05d167020cb0f14630c2733d1459805658
May Phuu Thwel <mayphuuthwel@parami.edu.mm>
2026-01-23 14:45:08 +0630

    Update p_app.py

--- p_app.py
+++ p_app.py
@@ -19,7 +19,7 @@ st.write("Logistic Regression Model with Pipeline")
 st.markdown("Please enter the behavioral details and time spent below to predict the personality.")
 
 
-logo_path = "mpt.jpg"
+logo_path = "mpt2.jpg"
 if os.path.exists(logo_path):
     st.sidebar.image(logo_path, width=150)
 
@@ -78,6 +78,7 @@ probability = model.predict_proba(input_df)[0][1]
 st.write("### Prediction Probability")
 st.progress(probability)
 st.write(f"{probability*100:.2f}% chance of being {prediction}")
+
 
 
 
